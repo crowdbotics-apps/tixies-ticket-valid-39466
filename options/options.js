b902b5f48917130f7a3e0a232fc8bc04538e3f36
Crowdbotics <team@crowdbotics.com>
2023-03-20 20:02:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tixies-ticket-valid-39466",
+  url: "https://tixies-ticket-valid-39466.botics.co",
+  api: "https://tixies-ticket-valid-39466.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
